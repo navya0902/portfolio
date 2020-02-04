5dd480cfdb0bf10c4b2a90462f9b0174cfd36666
M Navya Sree <navya.sree@enquero.com>
2020-02-04 23:26:40 +0530

    Portfolio

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+const navya = baffle(".what_do");
+navya.set({
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed:120
+});
+navya.start();
+navya.reveal(4000);
+
+const navyaman = baffle(".headingtext");
+navyaman.set({
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed:120
+});
+navyaman.start();
+navyaman.reveal(4000);
+
+const iamnavya = baffle(".heading_subtext");                                                                                                                    
+iamnavya.set({
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed:120
+});
+iamnavya.start();
+iamnavya.reveal(4000);
